bf704265cd7682d0b9df031f4b8539f842f27b96
Cherif53 <68652830+Cherif53@users.noreply.github.com>
2026-03-01 20:20:18 +0100

    mise à jour data source

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -4,19 +4,14 @@ import { use } from 'passport';
 
 config();
 const isProd = process.env.NODE_ENV === 'production';
-const useSSL = process.env.POSTGRES_SSL === 'true' || isProd;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  ssl: useSSL ? {
+  url: process.env.DATABASE_URL, // pour Heroku
+  ssl: isProd ? {
     rejectUnauthorized: false,
   } : false,
-  database: process.env.POSTGRES_DB || 'skillmatch',
   synchronize: process.env.NODE_ENV !== "production",
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
 });
